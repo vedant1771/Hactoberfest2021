404097ad3bd78c3d48942cb34ca3a4f4fbe83724
vishmay-k <vkarbotkar7@gmail.com>
2021-10-03 11:44:17 +0530

    improvised Search.py code

--- Search.py
+++ Search.py
@@ -1,12 +1,8 @@
-word = 'Hello World'
+paragraph = 'A snake charmer is a person who moves the streets with different types of the banks of the river Yamuna. It is snakes in his basket. He goes from one place to another to show various types of snakes and their tricks. He carries a pipe with which he plays music and snakes dance to his tune. He usually wears a colourful dress. The job of a snake charmer is quite dangerous. Some snakes are quite poisonous and can even bite him. It is not an easy task to catch and train them for the shows.'
 
-result = word.find('hello')
-print ("Substring 'hello' found at index:", result )
 
-result = word.find('World')
-print ("Substring 'World' found at index:", result )
-
-if (word.find('welcome') != -1):
-	print ("Contains given substring ")
+if (paragraph.find('colourful') != -1):
+	result = paragraph.find('colourful')
+	print ("Substring found at index:", result )
 else:
-	print ("Doesn't contains given substring")
+	print ("The paragraph doesn't contains given substring")
