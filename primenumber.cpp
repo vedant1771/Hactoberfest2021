02af4d84dda11f1addc40772beabe5773cf763a6
Saurabh-del <gautamsaurabh199@gmail.com>
2021-10-03 19:34:26 +0530

    prime number code optimised

--- primenumber.cpp
+++ primenumber.cpp
@@ -5,12 +5,18 @@ int main()
   int n, i, m=0, flag=0;  
   cout << "Enter the Number to check Prime: ";  
   cin >> n;  
-  m=n/2;  
-  for(i = 2; i <= m; i++)  
+   
+  if(n<2)
+  {
+     cout<<"Number is not Prime."<<endl;  
+     return 0;
+  }
+   
+  for(i = 2; i*i <= n; i++)  
   {  
       if(n % i == 0)  
       {  
-          cout<<"Number is not Prime."<<endl;  
+          
           flag=1;  
           break;  
       }  
