f26086383f1a1922130aa720b9aabab5ea7eda0d
Abhishek raut <56253046+abhishek23012000@users.noreply.github.com>
2021-10-06 00:06:05 +0530

    updated kadane's_Algo.cpp
    my version of kadane's algorithm

--- kadane's_Algo.cpp
+++ kadane's_Algo.cpp
@@ -4,15 +4,13 @@ using namespace std;
  
 int maxSubArraySum(int a[], int size)
 {
-   int maxi = a[0];
-   int curx = a[0];
- 
-   for (int i = 1; i < size; i++)
-   {
-        curx = max(a[i], curx+a[i]);
-        maxi = max(maxi, curx);
-   }
-   return maxi;
+	int max_so_far = 0, curr_max = Integer.MIN_VALUE;
+    for(int i: arr){
+    	max_so_far += i;
+        if(max_so_far<0) max_so_far = 0;
+        if(max_so_far>curr_max) curr_max = max_so_far;
+    }
+    return curr_max;
 }
  int main()
 {
