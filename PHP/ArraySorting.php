27b1b85aebe62656779615aae26716eedc439d7c
Rishabh Dhawad <rishabhdhawad29@gmail.com>
2021-10-07 15:18:01 +0530

    Added Some Basic PHP programming

---
+++ PHP/ArraySorting.php
@@ -0,0 +1,14 @@
+<?php
+// Array Sorting in Alphabets
+$cars = array("BMW" , "Mercedes" , "Audi");
+
+sort ($cars);
+print_r ($cars);
+
+
+// Array Sorting in Number
+$number = array(1,5,8,9,34,2,6,);
+sort($number);
+print_r ($number);
+
+?>
---
+++ PHP/SwitchCase.php
@@ -0,0 +1,42 @@
+<html>
+<body>
+
+<?php
+
+$day = "7";
+switch ($day) {
+    case "1";
+    echo "My favorite clour is Red";
+    break;
+
+    case "2";
+    echo "My favorite clour is Red";
+    break;
+
+    case "3";
+    echo "My favorite clour is Red";
+    break;
+
+    case "4";
+    echo "My favorite clour is Red";
+    break;
+
+    case "5";
+    echo "My favorite clour is Red";
+    break;
+
+    case "6";
+    echo "My favorite clour is Red";
+    break;
+
+    case "7";
+    echo "My favorite clour is Red";
+    break;
+
+    default:
+    echo "Invalid Number";
+}
+
+?>
+</body>    
+</html>
