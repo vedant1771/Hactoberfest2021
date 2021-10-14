879752bb78c530f9e0de94cf1cf4917a59b0f39e
ilyas829 <72152969+ilyas829@users.noreply.github.com>
2021-10-14 11:45:40 +0530

    Improved code
    There was unnecessary use of pointer in function which will slow down the program , also the I moved the Function before the main function I have also added comment which will help in understanding the code!

--- C/swap.c
+++ C/swap.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-void swap(int *a, int *b);
-
+void swap(int a, int b) //function to swap two numbers
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 int main()
 {
     printf("Enter two numbers: \n");
     int m,n;
+    printf("m=");
     scanf("%d",&m);
+    printf("n=");
     scanf("%d",&n);
-    printf("m = %d\n", m);
-    printf("n = %d\n\n", n);
 
-    swap(&m, &n);    
+    swap(m, n);    //Function call
     printf("Numbers after Swapping:\n\n");
     printf("m = %d\n", m);
     printf("n = %d", n);
     return 0;
-}
-
-
-void swap(int *a, int *b) 
-{
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
 }
