b89a5dee851ab0429e72e7b226d658f98d6c4241
Winner121353 <50233676+Winner121353@users.noreply.github.com>
2021-10-17 23:50:45 +0530

    Update 01 Print number in words.cpp
    Typo and Indentation fixed. If satisfying then merge.

--- C++/01 Print number in words.cpp
+++ C++/01 Print number in words.cpp
@@ -34,40 +34,40 @@ eight
 
  
 #include<iostream>
-using namespace std ;
+using namespace std;
 int main ()
 {
 	int no,i,a[100];
 	cin>>no; // taking input from user 
 	for (i=1;i<=no;i++)
-	cin>>a[i]; // scanning throuh the element passed by user ;
+	cin>>a[i]; // scanning through the element passed by the user
 	for (i=1;i<=no;i++)
 	{
 		if (a[i]>=1 && a[i]<=10)  // Validating condition 
 		{
 		if(a[i]==1)
-	    cout<<"one\n";
-	    else if(a[i]==2)
-	      cout<<"two\n";
-	    else if(a[i]==3)
-	      cout<<"three\n";
-	    else if(a[i]==4)
-	      cout<<"four\n";
-	    else if(a[i]==5)
-	      cout<<"five\n";
-	    else if(a[i]==6)
-	      cout<<"six\n";
-	    else if(a[i]==7)
-	      cout<<"seven\n";
-	    else if(a[i]==8)
-	      cout<<"eight\n";
-	    else if(a[i]==9)
-	      cout<<"nine\n";
-	    else if(a[i]==10)
-	      cout<<"ten\n";
+	          cout<<"one\n";
+	        else if(a[i]==2)
+	          cout<<"two\n";
+	        else if(a[i]==3)
+	          cout<<"three\n";
+	        else if(a[i]==4)
+	          cout<<"four\n";
+	        else if(a[i]==5)
+	          cout<<"five\n";
+	        else if(a[i]==6)
+	          cout<<"six\n";
+	        else if(a[i]==7)
+	          cout<<"seven\n";
+	        else if(a[i]==8)
+	          cout<<"eight\n";
+	        else if(a[i]==9)
+	          cout<<"nine\n";
+	        else if(a[i]==10)
+	          cout<<"ten\n";
 		}
 		else 
 		cout<<"not in range\n";
 	}
-return 0;
+ return 0;
 }
