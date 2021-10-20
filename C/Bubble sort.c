92ea7c1249bde7aaba74bc6a471a6a3745669a8c
sarangkj <56405345+sarangkj@users.noreply.github.com>
2021-10-20 20:49:37 +0530

    Update Bubble sort.c

--- C/Bubble sort.c
+++ C/Bubble sort.c
@@ -1,46 +1,34 @@
-
-#include <stdio.h>
-#include<time.h>
-#include<stdlib.h>
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-
-void bubbleSort(int arr[], int n)
-{
-int i, j;
-for (i = 0; i < n-1; i++)	
-
-	// Last i elements are already in place
-	for (j = 0; j < n-i-1; j++)
-		if (arr[j] > arr[j+1])
-			swap(&arr[j], &arr[j+1]);
-}
-
+#include<stdio.h>
 
 int main()
 {
-    clock_t start,end;
-    int i, n, lower=-65536, upper=65536;
-    
-    printf("Enter total no.of elements: ");
-    scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
+  int array[100], n, c, d, swap;
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+
+  for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
     {
-        a[i]=(rand()%(upper-lower+1))+lower;
+      if (array[d] > array[d+1]) /* For decreasing order use '<' instead of '>' */
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
     }
+  }
 
-    start=clock();
-	bubbleSort(a, n);
-    end=clock();
+  printf("Sorted list in ascending order:\n");
 
-    double diff=end-start;
-    double time=(double)diff/(CLOCKS_PER_SEC);
-    printf("\n The time to execute this program is %lf",time);
-    return 0;
+  for (c = 0; c < n; c++)
+     printf("%d\n", array[c]);
+
+  return 0;
 }
