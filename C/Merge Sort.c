1aa066aa0654d281742689ad74c325ed7a3229b9
Shubham Prakash <89865676+Shubhamprakash007@users.noreply.github.com>
2021-10-21 16:42:49 +0530

    Update Merge Sort.c

--- C/Merge Sort.c
+++ C/Merge Sort.c
@@ -1,55 +1,49 @@
-#include <stdio.h>
-
-#define max 10
-
-int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
-int b[10];
-
-void merging(int low, int mid, int high) {
-   int l1, l2, i;
-
-   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
-      if(a[l1] <= a[l2])
-         b[i] = a[l1++];
-      else
-         b[i] = a[l2++];
-   }
-   
-   while(l1 <= mid)    
-      b[i++] = a[l1++];
-
-   while(l2 <= high)   
-      b[i++] = a[l2++];
-
-   for(i = low; i <= high; i++)
-      a[i] = b[i];
+#include<stdio.h>
+int merge(int a[],int l,int mid,int h){
+	
+	int i=l,j=mid+1,k=l;
+	int b[20];
+	
+	while(i<=mid&&j<=h){
+		if(a[i]<a[j]){
+			b[k++]=a[i++];
+		}
+		else
+		   b[k++]=a[j++];
+	}
+	for(;i<=mid;i++)
+		b[k++]=a[i];
+	for(;j<=h;j++)
+	    b[k++]=a[j];
+	    
+	for(i=l;i<=h;i++)
+	a[i]=b[i];
+}
+	
+int mergesort(int a[], int l, int h){
+	if(l<h){
+		 int mid;
+		 mid=(l+h)/2;
+		mergesort(a,l,mid);
+		mergesort(a,mid+1,h);
+		merge(a,l,mid,h);
+	}
 }
 
-void sort(int low, int high) {
-   int mid;
-   
-   if(low < high) {
-      mid = (low + high) / 2;
-      sort(low, mid);
-      sort(mid+1, high);
-      merging(low, mid, high);
-   } else { 
-      return;
-   }   
-}
 
-int main() { 
-   int i;
-
-   printf("List before sorting\n");
-   
-   for(i = 0; i <= max; i++)
-      printf("%d ", a[i]);
-
-   sort(0, max);
-
-   printf("\nList after sorting\n");
-   
-   for(i = 0; i <= max; i++)
-      printf("%d ", a[i]);
+int main(){
+	int a[20],n,i;
+	printf("enter size you want for array :");
+	scanf("%d",&n);
+	
+	printf(" enter elemets of array :");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	
+	mergesort(a,0,n-1);
+    printf("sorted array :");
+	for(i=0;i<n;i++){
+		printf("\t%d",a[i]);
+	}		
 }
