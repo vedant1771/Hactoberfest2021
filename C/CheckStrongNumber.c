ed87e9deb78682adee715fc54d6e11f3a9208506
Shivangi <65915506+shivangi11012000@users.noreply.github.com>
2021-10-27 13:38:32 +0530

    Create CheckStrongNumber.c

---
+++ C/CheckStrongNumber.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+int n,i;
+int fact,rem;
+printf("\nEnter a number : ");
+scanf("%d",&n);
+printf(“\n”);
+int sum = 0;
+int temp = n;
+while(n)
+{
+i = 1,fact = 1;
+rem = n % 10;
+
+while(i <= rem)
+{
+fact = fact * i;
+i++;
+}
+sum = sum + fact;
+n = n / 10;
+}
+if(sum == temp)
+printf("%d is a strong number\n",temp);
+else
+printf("%d is not a strong number\n",temp);
+
+return 0;
+}
